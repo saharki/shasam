c660cd114c05b101c0d625f753da77fa62da9526
sahar.ki <sahar.ki@gmail.com>
2016-01-09 13:57:42 +0200

    GUI 1

--- AudioTranscription/AudioTranscription/ResultWindow.Designer.cs
+++ AudioTranscription/AudioTranscription/ResultWindow.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace AudioTranscription
 {
-    partial class Form1
+    partial class ResultWindow
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // ResultWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(908, 567);
+            this.Name = "ResultWindow";
+            this.Text = "Shasam";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
     }
 }
-
--- AudioTranscription/AudioTranscription/ResultWindow.cs
+++ AudioTranscription/AudioTranscription/ResultWindow.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace AudioTranscription
 {
-    public partial class Form1 : Form
+    public partial class ResultWindow : Form
     {
-        public Form1()
+        public ResultWindow()
         {
             InitializeComponent();
         }
