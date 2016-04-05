be349dfb33f8f8ae9731b712ed399d0330f5d390
Sahar <sahar>
2016-04-05 16:38:41 +0300

    Fixed threshold function

---
+++ AudioTranscription/AudioTranscription/Thresholding.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioTranscription
+{
+    class Thresholding
+    {
+        public static double[] FixedThreshold( double[] arr, double threshold)
+        {
+            double[] result = new double[arr.Length];
+            for(int i=0;i<arr.Length;i++)
+            {
+                if (arr[i] < threshold)
+                    result[i] = 0;
+                else
+                    result[i] = arr[i];
+            }
+            return result;
+        }
+    }
+}
