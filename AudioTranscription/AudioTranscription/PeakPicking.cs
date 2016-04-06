77fd76c91cced2f9a8b039dee7af2dca64719225
Sahar <sahar>
2016-04-06 09:54:34 +0300

    LPF for Peak Picking (need checking)

---
+++ AudioTranscription/AudioTranscription/PeakPicking.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioTranscription
+{
+    class PeakPicking
+    {
+        public static double[] LPF(double []x, double dt,double RC)
+        {
+            double[] y = new double[x.Length];
+            double a = dt / (RC + dt);
+            y[0] = x[0];
+            for(int i=1;i<x.Length;i++)
+            {
+                y[i] = a * x[i] + (1 - a) * y[i - 1];
+            }
+            return y;
+        }
+    }
+}
